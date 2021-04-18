712fcf271b8fb89cd278fb72a7b2843bdfa7c923
Fabia <Fabia@DESKTOP-KUGMPH2>
2021-04-18 17:27:06 -0500

    Nueva carpeta inicio

---
+++ Banco/src/Inicio/inicio.java
@@ -0,0 +1,12 @@
+package Inicio;
+
+import javax.swing.JOptionPane;
+
+public class inicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  JOptionPane.showMessageDialog(null, "Holis");
+	}
+
+}
