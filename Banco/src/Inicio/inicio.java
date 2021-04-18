341ec4b23ae17abe978f7d3341acb508e712277e
NicolasD1440 <82782749+NicolasD1440@users.noreply.github.com>
2021-04-18 17:32:12 -0500

    Update inicio.java

--- Banco/src/Inicio/inicio.java
+++ Banco/src/Inicio/inicio.java
@@ -6,7 +6,8 @@ public class inicio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  JOptionPane.showMessageDialog(null, "Holis");
+         JOptionPane.showMessageDialog(null, "Holis");
+		System.out.println("Edicion desde github");
 	}
 
 }
