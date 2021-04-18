d715d5cb039954ea07c2ddd43593617d4e33075d
Fabia <Fabia@DESKTOP-KUGMPH2>
2021-04-18 18:26:15 -0500

    nueva edicion

---
+++ Banco/src/Inicio/inicio.java
@@ -0,0 +1,13 @@
+package Inicio;
+
+import javax.swing.JOptionPane;
+
+public class inicio {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         JOptionPane.showMessageDialog(null, "Holis");
+         JOptionPane.showMessageDialog(null, "Holis 2");
+         JOptionPane.showMessageDialog(null, "Holis 3");
+	
+}
+}
