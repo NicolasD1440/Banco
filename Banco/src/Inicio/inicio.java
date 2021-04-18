5dee678f66408685ac262cd1feb25f8d9fa26717
CRISTIAN CRUZ <CRISTIAN CRUZ@INTEL>
2021-04-18 18:04:36 -0500

    mi aporte

--- Banco/src/Inicio/inicio.java
+++ Banco/src/Inicio/inicio.java
@@ -7,7 +7,7 @@ public class inicio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          JOptionPane.showMessageDialog(null, "Holis");
-         JOptionPane.showMessageDialog(null, "Holis x2");
+         JOptionPane.showMessageDialog(null, "Holis 2");
 		
 	}
 
