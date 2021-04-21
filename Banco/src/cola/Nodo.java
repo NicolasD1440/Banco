94010efc2e5a9d0d460886c8b460e8e8635ba647
XxByDaniielxX <johan.chavez@uniminuto.edu.co>
2021-04-21 12:18:59 -0500

    Comentarios nodo

--- Banco/src/cola/Nodo.java
+++ Banco/src/cola/Nodo.java
@@ -2,9 +2,11 @@ package cola;
 
 public class Nodo {
 	
+	  //Atributos
 	  private Turnos informacion;//Aqui se cambia por el objeto
-	  private Nodo referencia;
-	
+	  private Nodo referencia;// Variable para enlazar los nodos.
+
+	//Respectivos get y set de los atributos
 	public Turnos getInformacion() {
 		return informacion;
 	}
