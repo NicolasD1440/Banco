245ccd59a7b11810df958be5d16e123159c7849f
CRISTIAN CRUZ <CRISTIAN CRUZ@INTEL>
2021-04-20 21:04:25 -0500

    modificaciones

--- Banco/src/cola/Turnos.java
+++ Banco/src/cola/Turnos.java
@@ -1,28 +1,17 @@
 package cola;
 
 public class Turnos {
-   int posicion;
    String cedula;
    String turno;
    String nombre;
    
-  public Turnos(int posicion,String cedula, String turno, String nombre) {
+  public Turnos(String cedula, String turno, String nombre) {
 	super();
-	this.posicion = posicion;
 	this.cedula = cedula;
 	this.turno = turno;
 	this.nombre = nombre;
 }
-
-public int getPosicion() {
-	return posicion;
-}
-
-public void setPosicion(int posicion) {
-	this.posicion = posicion;
-}
-
-
+  
 public String getCedula() {
 	return cedula;
 }
